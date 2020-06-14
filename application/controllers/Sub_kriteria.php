82a44b5d89f15c3b0bd8f9450923440915d0bc57
syahandri <syah.andri1406@gmail.com>
2020-06-14 13:54:34 +0700

    fix sub kriteria controller

--- application/controllers/Sub_kriteria.php
+++ application/controllers/Sub_kriteria.php
@@ -79,6 +79,14 @@ class Sub_kriteria extends CI_Controller {
 
     public function getKodeKriteria () {
         $data = $this->Sub_kriteria_Model->getKodeKriteria();
+
+        $index = 0;
+        $array[] = [
+            "kode_kriteria" => "",
+            "kriteria" => "--- Pilih Salah Satu ---"
+        ];
+
+        array_splice($data, $index, 0 , $array);
         echo json_encode($data);
     }
 
