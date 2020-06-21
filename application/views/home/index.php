c9cf45723e10061c272fecfb00c52a41fcf8ca49
syahandri <syah.andri1406@gmail.com>
2020-06-21 20:23:36 +0700

    fixing home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,49 +1,54 @@
-<div class="container">
+<div class="container-fluid">
+
     <!-- info -->
     <div class="row mb-4">
         <div class="col-md-4">
             <div class="card border-info">
                 <div class="card-header bg-info" style="color: white"><i class="fas fa-fw fa-users"></i>
-                    <span>Jumlah Karyawan</span></div>
+                    <span class="font-weight-bold">Jumlah Karyawan</span></div>
                 <div class="card-body">
                     <h3 class="card-title jml-karyawan"></h3>
-                    <a href="<?= base_url('karyawan'); ?>" class="badge badge-success">Lihat detail <i
+                </div>
+                <div class="card-footer">
+                    <a href="<?= base_url('karyawan'); ?>" class="badge badge-success float-right">Lihat detail <i
                             class="fas fa-fw fa-chevron-circle-right"></i> </a>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card border-info">
-                <div class="card-header bg-info" style="color: white"><i class="fas fa-fw fa-bars"></i> <span>Jumlah
-                        Kriteria</span></div>
+                <div class="card-header bg-info" style="color: white"><i class="fas fa-fw fa-bars"></i>
+                    <span class="font-weight-bold">Jumlah Kriteria</span></div>
                 <div class="card-body">
                     <h3 class="card-title jml-kriteria"></h3>
-                    <a href="<?= base_url('kriteria'); ?>" class="badge badge-success">Lihat detail <i
-                            class="fas fa-fw fa-chevron-circle-right"></i> </a>
-
                 </div>
+                <div class="card-footer">
+                    <a href="<?= base_url('kriteria'); ?>" class="badge badge-success float-right">Lihat detail <i
+                            class="fas fa-fw fa-chevron-circle-right"></i> </a></div>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card border-info">
                 <div class="card-header bg-info" style="color: white"><i class="fas fa-fw fa-caret-down"></i>
-                    <span>Jumlah Sub Kriteria</span></div>
+                    <span class="font-weight-bold">Jumlah Sub Kriteria</span></div>
                 <div class="card-body">
                     <h3 class="card-title jml-sub"></h3>
-                    <a href="<?= base_url('sub_kriteria'); ?>" class="badge badge-success">Lihat detail <i
+                </div>
+                <div class="card-footer">
+                    <a href="<?= base_url('sub_kriteria'); ?>" class="badge badge-success float-right">Lihat detail <i
                             class="fas fa-fw fa-chevron-circle-right"></i> </a>
-
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- chart -->
     <div class="row">
+
+        <!-- chart -->
         <div class="col-md-5">
             <div class="card border-primary">
                 <div class="card-header bg-primary" style="color: white"><i class="fas fa-fw fa-chart-bar"></i>
-                    <span>Grafik Hasil Penilaian</span>
+                    <span class="font-weight-bold">Grafik Hasil Penilaian</span>
                     <div class="col-md-5 float-right">
                         <select class="form-control" id="filterTgl" name="filterTgl"></select>
                     </div>
@@ -54,10 +59,12 @@
                 </div>
             </div>
         </div>
+
+        <!-- tabel hasil penilaian -->
         <div class="col-md-7">
             <div class="card border-primary">
                 <div class="card-header bg-primary" style="color: white"><i class="fas fa-fw fa-server"></i>
-                    <span>Tabel Hasil Penilaian</span>
+                    <span class="font-weight-bold">Tabel Hasil Penilaian</span>
                 </div>
                 <div class="card-body">
                     <table class="table display responsive nowrap" style="width:100%" id="tablePenilaianHome">
